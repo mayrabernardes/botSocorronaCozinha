fa1b0f96d8d1b8ffb59e27c3ac1af2da3eb25f6f
mayrabernardes <mayra.bernardesc@outlook.com>
2018-12-07 09:20:07 -0200

    Exclusao de arquivos nao utilizados

---
+++ README.txt
@@ -0,0 +1,3 @@
+Projeto realizado para a disciplina de Interação Humano-Computador
+Faculdade de Tecnologia - Curso de Análise e Desenvolvimento de Sistemas
+Bot para Telegram
--- tentativa_bottery.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup as bs
-
-url = requests.get('https://receitas.ig.com.br/busca/?q=arroz+de+forno.html')
-s = bs(url.content,'html.parser')
-ricos = s.findAll('ul',{'class': 'receitas'})
-
-for x in ricos:
-    print(x)
